ae62ba7b226a1ca6616d4fd0378200c087fa583c
saphirevert <jerome.cosandey@gmail.com>
2020-09-16 12:19:29 +0200

    Nouveau nettoyage du code

--- index.js
+++ index.js
@@ -34,35 +34,15 @@ bot.on([/^\/last$/, /^\/last (.+)$/], (msg, props) => {
   } else {
     nbPage = Number(props.match[1])
   }
-  fetch('https://about.gitlab.com/atom.xml')
-    .then((res) => res.text())
-    .then((body) => {
-      // console.log(body)
-      parseString(body, function (err, result) {
-        // console.dir(result);
-        // console.log(result.feed.entry[0]);
-        // console.log(result.feed.entry[0].content[0]._);
-        // console.log(props.match[1]);
-        // msg.reply.text(result.feed.entry[0].title[0]);
-        // msg.reply.text(result.feed.entry[0].content[0]._);
-        for (i = 0; i < nbPage; i++) {
-          console.log(i)
-          msg.reply.text(
-            result.feed.entry[i].title[0] +
-              '\nAuthor: ' +
-              result.feed.entry[i].author[0].name[0] +
-              '\n' +
-              result.feed.entry[i].link[0].$.href
-          )
-          // msg.reply.text(result.feed.entry[i].author[0].name[0]);
-          // msg.reply.text(result.feed.entry[i].link[0].$.href);
-          // msg.reply.text(result.feed.entry[i].author.name);
-          // msg.reply.text(result.feed.entry[i].link[0].href);
-          // console.log(result.feed.entry[i].link[0].$.href);
-          // console.dir(result.feed.entry[i].author[0].name[0]);
-        }
-      })
-    })
+  for (i = 0; i < nbPage; i++) {
+      msg.reply.text(
+          lastxml.feed.entry[i].title[0] +
+          '\nAuthor: ' +
+          lastxml.feed.entry[i].author[0].name[0] +
+          '\n' +
+          lastxml.feed.entry[i].link[0].$.href
+      )
+  }
 })
 
 bot.on('/notify', (msg) =>
