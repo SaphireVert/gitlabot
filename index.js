eee3b29273b969b11a06456289d3f41ef6e04c06
saphirevert <jerome.cosandey@gmail.com>
2020-09-16 12:19:42 +0200

    Nettoyage

--- index.js
+++ index.js
@@ -11,23 +11,19 @@ const TeleBot = require('telebot')
 const bot = new TeleBot(BOT_TOKEN)
 const fetch = require('node-fetch')
 var parseString = require('xml2js').parseString
-msg_sample = 'totomsg'
-var fichier_a_comparer = ''
-var reference = []
-var diff = []
 var lastxml = []
+// var tmp_msg_sample = fs.readFileSync('./msg_sample.json')
+// msg_sample = JSON.parse(tmp_msg_sample)
+// console.log(msg_sample);
 
 
-async function getResults() {
-  return await couteauSuisse.request()
-}
 
 bot.on('/start', (msg) => msg.reply.text('Welcome to gitlabot ! Type /help to get some help.'))
 bot.on('/help', (msg) =>
   msg.reply.text('Commands list:\n/start: Start the bot \n/help: Display the command list')
 )
 // bot.on('/last' , (msg) => {
-bot.on([/^\/last$/, /^\/last (.+)$/], (msg, props) => {
+bot.on([/^\/last$/, /^\/last (.+)$/], function test (msg, props) {
   var nbPage
   if (typeof props.match[1] === 'undefined') {
     nbPage = 1
@@ -50,109 +46,40 @@ bot.on('/notify', (msg) =>
 )
 
 bot.on([/^\/release$/, /^\/release (.+)$/], async (msg, props) => {
+  // console.log(msg);
+  console.log('oui !!!')
   var nbPage
   if (typeof props.match[1] === 'undefined') {
     nbPage = 1
   } else {
     nbPage = Number(props.match[1])
   }
-  var result = await couteauSuisse.request()
-  // console.log(result);
-
-  for (var i = 0; i < nbPage; i++) {
-    // console.log(i);
-    var sentence = result.feed.entry[i].title[0]
-    var word = 'elease'
-    if (sentence.includes(word)) {
-      console.log('oui !!!')
-      msg.reply.text(
-        result.feed.entry[i].title +
-          '\nAuthor: ' +
-          result.feed.entry[i].author[0].name[0] +
-          '\n' +
-          result.feed.entry[i].link[0].$.href
-      )
-    }
-    console.log(result.feed.entry[i].title)
-  }
-})
-
-// bot.on('/notify', (msg) => {
-//     return msg.reply.sticker('http://i.imgur.com/VRYdhuD.png', { asReply: true });
-// });
-
-//lastxml
-
-/*
-    1. On obtient le fichier distant
-    2. On prendre la première entrée du fichier distant
-    3. On regarde si elle existe dans le fichier local
-    3a. Si oui, on ne fait rien du tout
-    3b. Si non, on remplace le fichier local avec le fichier distant
-*/
-async function cronWork() {
-  console.log('Function start')
-  lastxml = await couteauSuisse.request()
-  // lastxml = lastxml.feed.id[0]
-  let tmpArray = []
-  // Thanks to https://stackoverflow.com/a/19084915/13715020
-  for (var i = 0; i < lastxml.feed.entry.length; i++) {
-    tmpArray.push(lastxml.feed.entry[i].id[0])
-  }
-  console.log('Flux RSS mis à jour')
-
-  // Thanks to https://stackoverflow.com/a/13523757/13715020
-  if (
-    !(
-      reference.length == tmpArray.length &&
-      reference.every(function (u, i) {
-        return u === tmpArray[i]
-      })
-    )
-  ) {
-    console.log('Le fichier a changé')
-    diff = []
-    for (var i = 0; i < reference.length; i++) {
-      // Thanks to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-      var found = tmpArray.find((element) => element == reference[i])
-      if (!found) {
-        diff.push(tmpArray[i])
+  let compteur = 0;
+  for (let i = 0; compteur < nbPage && i < lastxml.feed.entry.length; i++) {
+      var sentence = lastxml.feed.entry[i].title[0]
+      var word = 'elease'
+      if (sentence.includes(word)) {
+          console.log('oui !!!')
+          compteur++;
+          msg.reply.text(
+              lastxml.feed.entry[i].title +
+              '\nAuthor: ' +
+              lastxml.feed.entry[i].author[0].name[0] +
+              '\n' +
+              lastxml.feed.entry[i].link[0].$.href
+          )
       }
-    }
-    diff = diff.reverse()
-    reference = tmpArray
-    console.log(diff)
   }
-  // bot.sendMessage()
-  console.log('End function')
-}
-
-cron.schedule('* * * * * *', () => {
-  cronWork()
+  console.log(compteur);
+  if (compteur == 0) {
+      msg.reply.text("No recent release found")
+  } else if (compteur < nbPage) {
+      msg.reply.text(compteur + " results displayed")
+  }
 })
 
-reference = [
-  'https://about.gitlab.com/blog/2020/09/10/cloud-native-storage-beginners/',
-  'https://about.gitlab.com/blog/2020/09/09/being-a-better-ally/',
-  'https://about.gitlab.com/blog/2020/09/08/gnome-follow-up/',
-  'https://about.gitlab.com/blog/2020/09/08/efficient-code-review-tips/',
-  'https://about.gitlab.com/releases/2020/09/04/gitlab-13-3-5-released/',
-  'https://about.gitlab.com/blog/2020/09/03/how-being-public-by-default-in-security-builds-trust/',
-  'https://about.gitlab.com/blog/2020/09/03/is-devops-for-designers/',
-  'https://about.gitlab.com/blog/2020/09/03/risk-mapping/',
-  'https://about.gitlab.com/blog/2020/09/02/imposter-syndrome-and-remote-work/',
-  'https://about.gitlab.com/releases/2020/09/02/security-release-gitlab-13-3-3-released/',
-  'https://about.gitlab.com/blog/2020/09/01/a-tale-of-two-editors/',
-  'https://about.gitlab.com/blog/2020/09/01/using-bazel-to-speed-up-gitlab-ci-builds/',
-  'https://about.gitlab.com/releases/2020/09/01/ci-minutes-update-free-users/',
-  'https://about.gitlab.com/blog/2020/08/31/how-to-configure-dast-full-scans-for-complex-web-applications/',
-  'https://about.gitlab.com/releases/2020/08/28/gitlab-13-3-2-released/',
-  'https://about.gitlab.com/blog/2020/08/27/applying-risk-management-to-remote-learning/',
-  'https://about.gitlab.com/blog/2020/08/27/measuring-engineering-productivity-at-gitlab/',
-  'https://about.gitlab.com/blog/2020/08/25/ten-devops-terms/',
-  'https://about.gitlab.com/releases/2020/08/25/gitlab-13-3-1-released/',
-  'https://about.gitlab.com/blog/2020/08/24/gitlab-achieves-kcsp-status/i',
-  'https://about.gitlab.com/releases/2020/08/22/gitlab-13-3-released/iiiiiiii',
-]
+cron.schedule('* * * * * *', async () => {
+  lastxml = await couteauSuisse.request()
+})
 
 bot.start()
