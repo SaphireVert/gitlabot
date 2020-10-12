dadfeea09dc8cad8c7d4e15bc9033eea789c6fb6
saphirevert <jerome.cosandey@gmail.com>
2020-10-12 14:49:03 +0200

    Fichier Users_Settings.js nettoyé

--- Users_Settings.js
+++ Users_Settings.js
@@ -4,11 +4,7 @@ const fetch = require("node-fetch")
 var xml2js = require("xml2js")
 var parser = new xml2js.Parser(/* options */)
 const fs = require("fs")
-// var secretsFile = Fs.readFileSync('./secrets.json', 'utf-8')
-// const secretsFileObj = JSON.parse(secretsFile)
-// const BOT_TOKEN = secretsFileObj.BOT_TOKEN
 const TeleBot = require("telebot")
-// const bot = new TeleBot(BOT_TOKEN)
 
 class Users_Settings {
     constructor(filePath) {
@@ -41,22 +37,17 @@ class Users_Settings {
         this[msg.id].settings.notify.idLastSend = value
         this.save()
     }
-    async addChat(msg){
+    async addChat(msg) {
         this[msg.from.id][msg.chat.id.toString()] = {
             notify: {
                 notifyMode: "off",
                 dayMonth: "",
                 dayWeek: "",
                 dayHour: "",
-                idLastSend: ""
-            }
+                idLastSend: "",
+            },
         }
-        console.log(this);
-        // console.log(this[msg.from.id][msg.chat.id.toString()].valueOf());
-        // this[msg.from.id][msg.chat.id.toString()].push('test', 'test2')
-        // this[msg.from.id][msg.chat.id.toString()] = array
-        // console.log(this[msg.from.id][msg.chat.id.toString()]);
-        // this.save(msg.from.id)
+        this.save()
     }
     async addUser(msg) {
         this[msg.from.id] = {
@@ -66,10 +57,11 @@ class Users_Settings {
                     dayMonth: "",
                     dayWeek: "",
                     dayHour: "",
-                    idLastSend: ""
-                }
-            }
+                    idLastSend: "",
+                },
+            },
         }
+        this.save()
     }
 
     async init(msg) {
@@ -78,40 +70,24 @@ class Users_Settings {
             console.log("Adding " + msg.from.username)
             await this.addUser(msg)
         }
-        if (!this[msg.from.id][msg.chat.id]){
+        if (!this[msg.from.id][msg.chat.id]) {
             await this.addChat(msg)
         }
     }
 
-    save(user) {
-        //     getChat
-        // }
-        console.log(this);
+    save() {
+        console.log(this)
         fs.writeFile("./users_settings.json", JSON.stringify(this, null, 2), function writeJSON(
             err
         ) {})
     }
 
-    reset(msg){
-        this[msg.id] = ''
+    reset(msg) {
+        this[msg.id] = ""
         this.save(msg.id)
     }
 
-    // getUserInfos(userID){
-    //     getChat
-    // }
-
     importData(filePath) {
-        // if (!fs.existsSync('./data')) {
-        //     fs.mkdirSync('./data')
-        // }
-        // if (!fs.existsSync('./data/users')) {
-        //     fs.mkdirSync('./data/users')
-        // }
-        // if (!fs.existsSync('./data/groups')) {
-        //     fs.mkdirSync('./data/groups')
-        //     // console.log("n'existe pas")
-        // }
         if (!fs.existsSync(filePath)) {
             console.log("File settings doesn't exists, creating it")
             fs.writeFileSync(filePath, "{}")
